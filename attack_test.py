808c5e33bf1e707fa9aef61b4d3d078d2fe07d95
XU Xilie <33574158+GodXuxilie@users.noreply.github.com>
2020-06-17 21:48:59 +0800

    Update attack_test.py

--- attack_test.py
+++ attack_test.py
@@ -52,38 +52,27 @@ model.load_state_dict(torch.load(args.model_path)['state_dict'])
 print('==> Evaluating Performance under White-box Adversarial Attack')
 
 loss, test_nat_acc = attack.eval_clean(model, test_loader)
+print('Natural Test Accuracy: {:.2f}%'.format(100. * test_nat_acc))
 if args.method == "dat":
     # Evalutions the same as DAT.
     loss, fgsm_acc = attack.eval_robust(model, test_loader, perturb_steps=1, epsilon=0.031, step_size=0.031,loss_fn="cent", category="Madry",rand_init=True)
+    print('FGSM Test Accuracy: {:.2f}%'.format(100. * fgsm_acc))
     loss, pgd20_acc = attack.eval_robust(model, test_loader, perturb_steps=20, epsilon=0.031, step_size=0.031 / 4,loss_fn="cent", category="Madry", rand_init=True)
+    print('PGD20 Test Accuracy: {:.2f}%'.format(100. * pgd20_acc))
     loss, cw_acc = attack.eval_robust(model, test_loader, perturb_steps=30, epsilon=0.031, step_size=0.031 / 4,loss_fn="cw", category="Madry", rand_init=True)
-    print(
-        'Natural Test Accuracy: %.2f | FGSM Test Accuracy: %.2f | PGD20 Test Accuracy: %.2f | CW Test Accuracy: %.2f |\n' % (
-            test_nat_acc,
-            fgsm_acc,
-            pgd20_acc,
-            cw_acc)
-    )
+    print('CW Test Accuracy: {:.2f}%'.format(100. * cw_acc))
 if args.method == 'trades':
     # Evalutions the same as TRADES.
     # wri : with random init, wori : without random init
     loss, fgsm_wori_acc = attack.eval_robust(model, test_loader, perturb_steps=1, epsilon=0.031, step_size=0.031,loss_fn="cent", category="Madry",rand_init=False)
+    print('FGSM without Random Start Test Accuracy: {:.2f}%'.format(100. * fgsm_wori_acc))
     loss, pgd20_wori_acc = attack.eval_robust(model,test_loader, perturb_steps=20, epsilon=0.031, step_size=0.003,loss_fn="cent",category="Madry",rand_init=False)
+    print('PGD20 without Random Start Test Accuracy: {:.2f}%'.format(100. * pgd20_wori_acc))
     loss, cw_wori_acc = attack.eval_robust(model,test_loader, perturb_steps=30, epsilon=0.031, step_size=0.003,loss_fn="cw",category="Madry",rand_init=False)
+    print('CW without Random Start Test Accuracy: {:.2f}%'.format(100. * cw_wori_acc))
     loss, fgsm_wri_acc = attack.eval_robust(model, test_loader, perturb_steps=1, epsilon=0.031, step_size=0.031,loss_fn="cent", category="Madry",rand_init=True)
+    print('FGSM with Random Start Test Accuracy: {:.2f}%'.format(100. * fgsm_wri_acc))
     loss, pgd20_wri_acc = attack.eval_robust(model,test_loader, perturb_steps=20, epsilon=0.031, step_size=0.003,loss_fn="cent",category="Madry",rand_init=True)
+    print('PGD20 with Random Start Test Accuracy: {:.2f}%'.format(100. * pgd20_wri_acc))
     loss, cw_wri_acc = attack.eval_robust(model,test_loader, perturb_steps=30, epsilon=0.031, step_size=0.003,loss_fn="cw",category="Madry",rand_init=True)
-
-    print(
-        'Natural Test Acc %.2f | FGSM without Random Start Test Acc %.2f | PGD20 without Random Start Test Acc %.2f | CW without Random Start Test Acc %.2f | FGSM with Random Start Test Acc %.2f | PGD20 with Random Start Test Acc %.2f | CW with Random Start Test Acc %.2f |\n ' % (
-            test_nat_acc,
-            fgsm_wori_acc,
-            pgd20_wori_acc,
-            cw_wori_acc,
-            fgsm_wri_acc,
-            pgd20_wri_acc,
-            cw_wri_acc
-        )
-    )
-
-
+    print('CW with Random Start Test Accuracy: {:.2f}%'.format(100. * cw_wri_acc))

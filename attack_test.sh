739e3d9139d27c913a713c032b7b4a7759d3872e
XU Xilie <33574158+GodXuxilie@users.noreply.github.com>
2020-06-18 00:01:15 +0800

    Update attack_test.sh

--- attack_test.sh
+++ attack_test.sh
@@ -4,3 +4,6 @@ CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN_madry' --depth 32 --mo
 CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_for_TRADES_models/fat_for_trades_wrn34-10_eps0.031_beta1.0.pth.tar' --method 'trades'
 CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_for_TRADES_models/fat_for_trades_wrn34-10_eps0.031_beta6.0.pth.tar' --method 'trades'
 CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_for_TRADES_models/fat_for_trades_wrn34-10_eps0.062_beta6.0.pth.tar' --method 'trades'
+
+CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN' --depth 58 --model_path './FAT_for_TRADES_models/fat_for_trades_wrn58-10_eps0.031_beta6.0.pth.tar' --method 'trades'
+CUDA_VISIBLE_DEVICES='0' python attack_test.py --net 'WRN' --depth 58 --model_path './FAT_for_TRADES_models/fat_for_trades_wrn58-10_eps0.062_beta6.0.pth.tar' --method 'trades'

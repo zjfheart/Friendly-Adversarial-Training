5c10a6f6e64adf4e997b5c448af1f79dc88d1d13
XU Xilie <33574158+GodXuxilie@users.noreply.github.com>
2020-06-20 13:09:26 +0800

    Update attack_test.sh

--- attack_test.sh
+++ attack_test.sh
@@ -7,3 +7,6 @@ python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_models/fat_for_
 
 python attack_test.py --net 'WRN' --depth 58 --model_path './FAT_models/fat_for_trades_wrn58-10_eps0.031_beta6.0.pth.tar' --method 'trades'
 python attack_test.py --net 'WRN' --depth 58 --model_path './FAT_models/fat_for_trades_wrn58-10_eps0.062_beta6.0.pth.tar' --method 'trades'
+
+python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_models/fat_for_mart_wrn34-10_eps0.031.pth.tar' --method 'trades'
+python attack_test.py --net 'WRN' --depth 34 --model_path './FAT_models/fat_for_mart_wrn34-10_eps0.062.pth.tar' --method 'trades'
